db4b9c4c799c982dfc840685b04b3bec5a00bd6a
Drixyz13 <mattmariller@gmail.com>
2025-05-15 18:11:51 +0200

    Initial commit with journalists and crypto analysis

---
+++ README.md
@@ -0,0 +1,48 @@
+# 📊 Ruby Data Analyst Scripts
+
+Welcome to this Ruby project where we explore **data analysis** through two scripts.
+
+All code is located in the `lib/` folder.
+
+---
+
+## 📁 Project Structure
+
+- `lib/00_journalists.rb` – Twitter handles analysis  
+- `lib/01_cryptocurrencies.rb` – Cryptocurrency data analysis
+
+---
+
+## 🐦 Script 1: Journalists Twitter Handles
+
+**File**: `lib/00_journalists.rb`  
+**How to run**:
+```bash
+ruby lib/00_journalists.rb
+
+### 🔍 Features
+
+✅ Count total handles  
+✅ Find the shortest handle  
+✅ List handles with exactly 5 characters (excluding @)  
+✅ Filter handles that start with a capital letter  
+✅ Sort handles alphabetically and by length  
+✅ Find the position of the handle @epenser  
+✅ Display the distribution of handle lengths
+
+---
+
+## 💸 Script 2: Cryptocurrency Analysis
+
+**File**: `lib/01_cryptocurrencies.rb`  
+**How to run**:
+```bash
+ruby lib/01_cryptocurrencies.rb
+
+### 📈 Features
+
+✅ Pair crypto names and prices into a hash  
+✅ Find the most expensive coin  
+✅ Find the cheapest coin  
+✅ Filter all coins under $6000  
+✅ Among them, find the coin with the highest price under $6000
---
+++ lib/00_journalists.rb
@@ -0,0 +1,56 @@
+# On crée une liste de handles Twitter de journalistes
+handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# Affichage d'un message de bienvenue
+puts "💬 Bienvenue dans l'analyse des journalistes Twitter !"
+# Affiche le nombre total de handles dans la liste
+puts "Il y a #{handles.length} handles dans cette liste."
+# Recherche du handle le plus court (en nombre de caractères)
+shortest_handle = handles.min_by { |handle| handle.length }
+# Affiche le handle le plus court et sa taille (en retirant le @)
+puts "🔎 Le handle le plus court est : #{shortest_handle} (#{shortest_handle.length - 1} caractères hors @ )"
+# Sélectionne les handles qui font exactement 5 caractères (hors @)
+five_char_handles = handles.select do |handle|
+    handle.length - 1 == 5
+end
+# Affiche combien de handles font 5 caractères, et quelques exemples
+puts "🔢 Il y a  #{five_char_handles.count} handles qui font exactement 5 caractères (hors @) ."
+puts "Exemples : #{five_char_handles.first(5).join(', ')}..."
+# Sélectionne les handles qui commencent par une majuscule après le @
+capitalized_handles = handles.select do |handle|
+    first_letter = handle[1]
+    first_letter == first_letter.upcase && first_letter.match?(/[A-Z]/)
+end
+# Sélectionne les handles qui commencent par une majuscule après le @
+puts "🔠 Il y a #{capitalized_handles.count} handles qui commencent par une majuscule."
+puts "Exemples : #{capitalized_handles.first(5).join(', ')}..."
+# Trie les handles par ordre alphabétique
+sorted_handles = handles.sort
+# Affiche les 5 premiers handles triés alphabétiquement
+puts "🔤 Voici les 5 premiers handles (ordre alphabétique) :"
+puts sorted_handles.first(5).join(', ')
+# Trie les handles du plus court au plus long
+sorted_by_size = handles.sort_by { |handle| handle.length }
+# Affiche les 5 handles les plus courts
+puts "📏 Voici les 5 plus petits handles :"
+puts sorted_by_size.first(5).join(', ')
+# Cherche la position du handle "@epenser"
+position_epenser = handles.index("@epenser")
+# Affiche la position si trouvé, sinon message d'erreur
+if position_epenser
+  puts "🔎 Le handle @epenser est à la position #{position_epenser} dans le tableau."
+  else
+  puts "❌ Le handle @epenser n'est pas dans la liste."
+end
+# Création d'un Hash pour compter le nombre de handles par longueur (hors @)
+distribution = Hash.new(0)
+# On remplit ce Hash avec les longueurs des handles
+handles.each do |handle|
+    length = handle.length - 1
+    distribution[length] += 1
+end
+# Affiche la répartition du nombre de handles par taille
+puts "📊 Répartition des tailles de handles (hors @) :"
+distribution.sort.each do |length, count|
+    puts "➡️  #{count} handle(s) de #{length} caractère(s)"
+end
